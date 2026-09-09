2e4a9a869eecb8812de56380aa1948838544e7e5
Kzinn <kauafelipe18102007@gmail.com>
2026-09-09 00:04:18 -0300

    Add new phase data entries in Fases.js

--- Fases.js
+++ Fases.js
@@ -6,15 +6,33 @@ window.phaseData = {
 		command: 'decodificar',
 		commandHint: 'acesso concedido somente com a palavra original',
 		answer: 'epifania',
-		hint: 'veja por outra perspectiva.'
+		hint: 'perspectiva.'
 	},
 	2: {
 		label: 'arquivo // 02 de 26',
 		eyebrow: 'segundo contato',
 		riddle: '000100  000001  010010  001011',
-		command: 'interpretar',
+		command: 'decodificar',
 		commandHint: 'acesso concedido somente com a palavra original',
 		answer: 'dark',
 		hint: 'binário.'
+	},
+	3: {
+		label: 'arquivo // 03 de 26',
+		eyebrow: 'terceiro contato',
+		riddle: 'vlohqflr',
+		command: 'decodificar',
+		commandHint: 'acesso concedido somente com a palavra original',
+		answer: 'silencio',
+		hint: 'cifra.'
+	},
+	4: {
+		label: 'arquivo // 04 de 26',
+		eyebrow: 'quarto contato',
+		riddle: '2-4  1-9  2-7  1-3',
+		command: 'decodificar',
+		commandHint: 'acesso concedido somente com a palavra original',
+		answer: 'fome',
+		hint: 'linhas e colunas.'
 	}
 };
