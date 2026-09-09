36a3f40bfed43f3088df18449a19e1c8d85835f8
Kzinn <kauafelipe18102007@gmail.com>
2026-09-08 21:47:35 -0300

    Add files via upload

---
+++ Fases.js
@@ -0,0 +1,20 @@
+window.phaseData = {
+	1: {
+		label: 'arquivo // 01 de 26',
+		eyebrow: 'primeiro contato',
+		riddle: 'ainafipe',
+		command: 'decodificar',
+		commandHint: 'acesso concedido somente com a palavra original',
+		answer: 'epifania',
+		hint: 'veja por outra perspectiva.'
+	},
+	2: {
+		label: 'arquivo // 02 de 26',
+		eyebrow: 'segundo contato',
+		riddle: '000100  000001  010010  001011',
+		command: 'interpretar',
+		commandHint: 'acesso concedido somente com a palavra original',
+		answer: 'dark',
+		hint: 'binário.'
+	}
+};
